3c5beab1353e871f175a70a3da1c40430654af09
Kaik KDS <kaikdorvalo@gmail.com>
2024-08-16 22:15:13 -0300

    update: migration run command

--- src/modules/user/domain/entities/user.entity.ts
+++ src/modules/user/domain/entities/user.entity.ts
@@ -2,8 +2,9 @@ import { ColdObservable } from "rxjs/internal/testing/ColdObservable";
 import { TableNames } from "src/shared/constants/table-names.constants";
 import { USER_TABLE } from "src/shared/constants/tables.constants";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import "dotenv/config"
 
-@Entity({ name: TableNames.USER })
+@Entity({ name: TableNames.USER, schema: process.env.DATABASE_PUBLIC_SCHEMA })
 export class User {
     @PrimaryGeneratedColumn({ name: USER_TABLE.ID })
     id: number;
