06136283c02b4bc7eb9b7eae968d78f084fa4fd7
kaikdorvalo <kaikdorvalo@gmail.com>
2024-08-16 21:27:45 -0300

    created: database module and connection

--- src/modules/database/infrastructure/providers/database.provider.ts
+++ src/modules/database/infrastructure/providers/database.provider.ts
@@ -1,26 +1,30 @@
 import { DataSources } from "src/shared/constants/datasources.constants";
 import { DataSource } from "typeorm";
 import 'dotenv/config'
+import { User } from "src/modules/user/domain/entities/user.entity";
+
+const dataSourceConfig = new DataSource({
+    type: 'postgres',
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT),
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    schema: process.env.DATABASE_PUBLIC_SCHEMA,
+    entities: [
+        User
+    ],
+    migrations: ["src/modules/database/infrastructure/migrations/*{.ts,.js}"],
+    synchronize: false,
+});
 
 export const databaseProviders = [
     {
         provide: DataSources.PUBLIC_DATASOURCE,
         useFactory: async () => {
-            const dataSource = new DataSource({
-                type: 'postgres',
-                host: process.env.DATABASE_HOST,
-                port: parseInt(process.env.DATABASE_PORT),
-                username: process.env.DATABASE_USERNAME,
-                password: process.env.DATABASE_PASSWORD,
-                database: process.env.DATABASE,
-                schema: process.env.DATABASE_PUBLIC_SCHEMA,
-                entities: [
-                    __dirname + '/../**/*.entity{.ts,.js}',
-                ],
-                synchronize: true,
-            });
-
-            return dataSource.initialize();
+            return dataSourceConfig.initialize();
         },
     },
 ];
+
+export default dataSourceConfig
