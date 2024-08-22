d1732e70a38c1ccb9add1f3dbeec67b0efad54b4
kaikdorvalo <kaikdorvalo@gmail.com>
2024-08-21 22:01:12 -0300

    create: guard

--- src/modules/card/presentation/controllers/card.controller.ts
+++ src/modules/card/presentation/controllers/card.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Post, UseFilters } from "@nestjs/common";
+import { Body, Controller, Post, UseFilters, UseGuards } from "@nestjs/common";
 import { CreateDeckDto } from "src/shared/dtos/card/create-deck.dto";
 import { GetCommanderUseCase } from "../../application/use-case/get-commander.use-case";
 import { HttpExceptionFilter } from "src/shared/exceptions-filter/http-exception.exception-filter";
 import { GenerateDeckUseCase } from "../../application/use-case/generate-deck.use-case";
 import { ApiBody, ApiResponse } from "@nestjs/swagger";
 import { CreateUserDto } from "src/shared/dtos/user/create-user.dto";
+import { AuthGuard } from "src/modules/auth/application/guards/auth.guard";
 
 @Controller('cards')
 @UseFilters(new HttpExceptionFilter())
+@UseGuards(AuthGuard)
 export class CardController {
 
     constructor(
