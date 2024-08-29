5f67ea08c6993c95ff7e6538679a4d3ae8999d78
kaikdorvalo <kaikdorvalo@gmail.com>
2024-08-28 21:10:29 -0300

    fix: return user without password

--- src/modules/user/application/use-case/get-user.use-case.ts
+++ src/modules/user/application/use-case/get-user.use-case.ts
@@ -1,14 +1,19 @@
 import { ResponseData } from "src/shared/utils/response-data";
 import { UserRepository } from "../../infrastructure/persistence/user.repository";
-import { HttpStatus } from "@nestjs/common";
+import { HttpStatus, Injectable } from "@nestjs/common";
+import { IUser } from "../interfaces/user.interface";
 
+@Injectable()
 export class GetUserUseCase {
     constructor(
         private readonly userRepository: UserRepository
-    ) { }
+    ) {
+        console.log(userRepository)
+    }
 
     async execute(id: string) {
-        const user = await this.userRepository.getUserById(id);
+        const user: IUser = await this.userRepository.getUserById(id);
+        user.password = undefined;
         return new ResponseData(
             HttpStatus.FOUND,
             user
--- src/modules/user/infrastructure/persistence/user.repository.ts
+++ src/modules/user/infrastructure/persistence/user.repository.ts
@@ -2,7 +2,9 @@ import { User, UserDocument } from "../../domain/schemas/user.schema";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { IUser } from "../../application/interfaces/user.interface";
+import { Injectable } from "@nestjs/common";
 
+@Injectable()
 export class UserRepository {
     constructor(
         @InjectModel(User.name) private readonly userModel: Model<User>
