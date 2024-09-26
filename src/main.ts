013a787b00282401ce6904d67e1cee0b90986ec6
Kaik KDS <kaikdorvalo@gmail.com>
2024-09-25 21:10:19 -0300

    chore: add all cors origins

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,22 @@ import "reflect-metadata";
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { json, urlencoded } from 'express';
+const cors = require('cors');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(json({ limit: '50mb' }));  // Ajuste o limite de tamanho do JSON conforme necessário
   app.use(urlencoded({ limit: '50mb', extended: true }));  // Ajuste o limite do URL-encoded
+
+  const options = {
+    origin: "*",
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
+  app.enableCors(options);
 
 
   const config = new DocumentBuilder()
